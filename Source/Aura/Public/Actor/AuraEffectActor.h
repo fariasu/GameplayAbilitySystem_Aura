c2d241b543390169551666c0e1fb72e818ef3ac2
Pablo Garcia Farias <pablogarciafarias@gmail.com>
2024-04-16 13:23:52 -0300

    Periodic mana potion

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -30,4 +30,7 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> PeriodicGameplayEffectClass;
 };
